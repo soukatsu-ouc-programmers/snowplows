b772696020a460ce24bc4046e2e495f0d1adafa3
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2018-09-12 01:06:04 +0900

    issue#53 北海道マップに雪だるま配置

--- Unity/RemoveSnow/Assets/Scripts/SnowBattle/ApartSnowman.cs
+++ Unity/RemoveSnow/Assets/Scripts/SnowBattle/ApartSnowman.cs
@@ -8,6 +8,12 @@ using UnityEngine;
 public class ApartSnowman : MonoBehaviour {
 
 	/// <summary>
+	/// 雪だるまの原型サイズ
+	/// </summary>
+	[SerializeField]
+	private Vector3 snowmanScale;
+
+	/// <summary>
 	/// 雪だるまプレハブ
 	/// </summary>
 	[SerializeField]
@@ -20,13 +26,19 @@ public class ApartSnowman : MonoBehaviour {
 	public void OnCollisionEnter(Collision other) {
 		if(other.gameObject.tag == "Bullet") {
 			// 元々あった合体済み雪だるまをRigidbody付きの雪だるまに差し替えて自然とバラバラになるようにする
-			Object.Instantiate(
+			var fixedSnowman = this.gameObject.transform.parent;
+			var apart = Object.Instantiate(
 				this.apartSnowman,
-				this.gameObject.transform.position,
+				fixedSnowman.position,
 				Quaternion.identity,
-				GameObject.Find("DosankoCity").transform
+				GameObject.FindGameObjectWithTag("Ground").transform
 			);
+
+			// 原型を削除
 			Object.Destroy(this.transform.parent.gameObject);
+
+			// バラバラ雪だるまのスケールを合わせる
+			apart.transform.localScale = fixedSnowman.localScale;
 		}
 	}
 
